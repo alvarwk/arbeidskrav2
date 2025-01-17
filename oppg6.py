795872a58f19aa98d4a11320b8fa423b2e4199bc
Alvar Kleppa <alvark@egde.no>
2025-01-17 16:22:43 +0100

    oppg 6

---
+++ oppg6.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 200)
+
+y = -x**2 - 5
+
+plt.plot(x, y, label=r'$f(x) = -x^2 - 5$', color='b')
+
+plt.title('Plot av funksjonen $f(x) = -x^2 - 5$')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+
+plt.legend()
+plt.show()
